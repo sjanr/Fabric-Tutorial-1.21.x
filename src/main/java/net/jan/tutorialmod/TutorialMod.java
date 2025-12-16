a614997edd829612520f3aa1f901426038bac389
sjanr <97484144+sjanr@users.noreply.github.com>
2025-12-16 15:25:18 -0800

    Succesfully added pink garnet item

--- src/main/java/net/jan/tutorialmod/TutorialMod.java
+++ src/main/java/net/jan/tutorialmod/TutorialMod.java
@@ -2,19 +2,18 @@ package net.jan.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.jan.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class TutorialMod implements ModInitializer {
-	public static final String MOD_ID = "templatemod";
+	public static final String MOD_ID = "tutorialmod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 	// test comment
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
+		ModItems.registerModItems();
 
 		LOGGER.info("Hello Fabric world!");
 	}
---
+++ src/main/java/net/jan/tutorialmod/item/ModItems.java
@@ -0,0 +1,26 @@
+package net.jan.tutorialmod.item;
+
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.jan.tutorialmod.TutorialMod;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+    public static final Item PINK_GARNET = registerItem("pink_garnet", new Item(new Item.Settings()));
+
+    private static Item registerItem(String name, Item item){
+        return Registry.register(Registries.ITEM, Identifier.of(TutorialMod.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        TutorialMod.LOGGER.info("Registering od Items for " + TutorialMod.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
+            entries.add(PINK_GARNET);
+        });
+
+    }
+}
