7b7dd75bd774b74a628694f9297b1bf642e69d94
sjanr <97484144+sjanr@users.noreply.github.com>
2025-12-16 15:31:37 -0800

    Added raw pink garnet item

--- src/main/java/net/jan/tutorialmod/item/ModItems.java
+++ src/main/java/net/jan/tutorialmod/item/ModItems.java
@@ -10,6 +10,7 @@ import net.minecraft.util.Identifier;
 
 public class ModItems {
     public static final Item PINK_GARNET = registerItem("pink_garnet", new Item(new Item.Settings()));
+    public static final Item RAW_PINK_GARNET = registerItem("raw_pink_garnet", new Item(new Item.Settings()));
 
     private static Item registerItem(String name, Item item){
         return Registry.register(Registries.ITEM, Identifier.of(TutorialMod.MOD_ID, name), item);
@@ -20,6 +21,7 @@ public class ModItems {
 
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
             entries.add(PINK_GARNET);
+            entries.add(RAW_PINK_GARNET);
         });
 
     }
