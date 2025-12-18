d6e75d9192ef470e587a93c0dc4a28f991ec0778
sjanr <97484144+sjanr@users.noreply.github.com>
2025-12-18 14:39:46 -0800

    Added raw/regular pink garnet blocks

--- src/main/java/net/jan/tutorialmod/TutorialMod.java
+++ src/main/java/net/jan/tutorialmod/TutorialMod.java
@@ -2,6 +2,7 @@ package net.jan.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.jan.tutorialmod.block.ModBlocks;
 import net.jan.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,7 +15,6 @@ public class TutorialMod implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		ModItems.registerModItems();
-
-		LOGGER.info("Hello Fabric world!");
+		ModBlocks.registerModBlocks();
 	}
 }
